413a3d0eca5f587153d8962247defdd196c26f4b
half-metal-mac <brockboss@gmail.com>
2023-02-06 11:40:17 -0800

    sveltekit + tw + diasyui

---
+++ src/routes/+page.svelte
@@ -0,0 +1,16 @@
+<script>
+    let text = `this is Sveltekit + Tailwind + DaisyUI`
+    let additionalText = `this is a template`
+</script>
+
+<div class="bg-primary">
+    <p>
+        {text}
+    </p>
+</div>
+
+<div class="bg-primary">
+    <p>
+        {additionalText}
+    </p>
+</div>
